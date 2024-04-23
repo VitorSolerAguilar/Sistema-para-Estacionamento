8e699b6a43a0de471d087a4d4b8cde3fb3749ffa
VitorSolerAguilar <vitorsoleraguilar49@gmail.com>
2024-04-23 17:27:36 -0300

    Implementação dos metodos classe Estacionamento

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -1,13 +1,9 @@
 ﻿using DesafioFundamentos.Models;
 
-// Coloca o encoding para UTF8 para exibir acentuação
-Console.OutputEncoding = System.Text.Encoding.UTF8;
-
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
 
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" + "Digite o preço inicial:");
 precoInicial = Convert.ToDecimal(Console.ReadLine());
 
 Console.WriteLine("Agora digite o preço por hora:");
